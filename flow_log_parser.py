8598b84dc8d7c6f3203dc0ed70f20d93dc41537b
Jeffrey <gladiacxtylish@gmail.com>
2025-02-23 23:02:08 -0800

    small class change

--- flow_log_parser.py
+++ flow_log_parser.py
@@ -15,114 +15,119 @@ UNTAGGED_FLOW_LOGS = 0
 
 LOOKUP = {}
 
+class FlowLogParser:
+    def parse_protocol_number(self):
+        """
+        Parse the protocol file protocol-numbers-1.csv and construct the mapping
+        """
+        with open(PROTOCOL_NUMBER_FILENAME) as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                number = int(row['Decimal'])
+                protocol = row['Keyword'].lower()
+                PROTOCOLS[number] = protocol
 
-def parse_protocol_number():
-    """
-    Parse the protocol file protocol-numbers-1.csv and construct the mapping
-    """
-    with open(PROTOCOL_NUMBER_FILENAME) as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            number = int(row['Decimal'])
-            protocol = row['Keyword'].lower()
-            PROTOCOLS[number] = protocol
+    def parse_lookup(self):
+        """
+        Parse the lookup file lookup_table.csv and construct the mapping
+        """
+        with open(LOOKUP_TABLE_FILENAME) as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                port = int(row['dstport'])
+                protocol = row['protocol'].lower()
+                if not port in LOOKUP:
+                    LOOKUP[port] = {}
 
-def parse_lookup():
-    """
-    Parse the lookup file lookup_table.csv and construct the mapping
-    """
-    with open(LOOKUP_TABLE_FILENAME) as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            port = int(row['dstport'])
-            protocol = row['protocol'].lower()
-            if not port in LOOKUP:
-                LOOKUP[port] = {}
+                LOOKUP[port][protocol] = row['tag']
 
-            LOOKUP[port][protocol] = row['tag']
+    def parse_flow_log(self):
+        """
+        Parse the flow log file flow_log.txt and calculate the results
+        """
+        global UNTAGGED_FLOW_LOGS
+        with open(FLOW_LOG_FILENAME) as file:
+            for line in file:
+                token = line.strip().split()
+                if len(token) < FLOW_LOG_TOKENS:
+                    print(f"Invalid line {line}")
+                    continue
+                
+                dstport = int(token[6])
+                protocol = PROTOCOLS[int(token[7])]
 
-def parse_flow_log():
-    """
-    Parse the flow log file flow_log.txt and calculate the results
-    """
-    global UNTAGGED_FLOW_LOGS
-    with open(FLOW_LOG_FILENAME) as file:
-        for line in file:
-            token = line.strip().split()
-            if len(token) < FLOW_LOG_TOKENS:
-                print(f"Invalid line {line}")
-                continue
+                # write to the port and protocol count
+                if not dstport in PORT_PROTOCOL_COUNT:
+                    PORT_PROTOCOL_COUNT[dstport] = {}
+                if not protocol in PORT_PROTOCOL_COUNT[dstport]:
+                    PORT_PROTOCOL_COUNT[dstport][protocol] = 0
+                PORT_PROTOCOL_COUNT[dstport][protocol] += 1
+                
+                # check if the tag exist in our mapping
+                if not dstport in LOOKUP or not protocol in LOOKUP[dstport]:
+                    print(f"Not known dstport {line}")
+                    UNTAGGED_FLOW_LOGS += 1
+                    continue
+
+                # lookup the tag
+                tag = LOOKUP[dstport][protocol]
+
+                if not tag in FLOW_LOGS:
+                    FLOW_LOGS[tag] = 0
+
+                # inrement the tag count
+                FLOW_LOGS[tag] += 1
+
+    def output_flow_log_result(self):
+        """
+        Write the tag count output to a file tag_count.csv
+        """
+        with open(TAG_COUNT_FILENAME, 'w') as csvfile:
+            fieldnames = ['Tag', 'Count']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for tag in FLOW_LOGS:
+                writer.writerow({'Tag': tag, 'Count': FLOW_LOGS[tag]})
             
-            dstport = int(token[6])
-            protocol = PROTOCOLS[int(token[7])]
+            # if untagged exist, the write the count for untagged
+            if UNTAGGED_FLOW_LOGS > 0:
+                writer.writerow({'Tag': 'Untagged', 'Count': UNTAGGED_FLOW_LOGS})
+        
+        print(f"Tag count written to {TAG_COUNT_FILENAME}")
 
-            # write to the port and protocol count
-            if not dstport in PORT_PROTOCOL_COUNT:
-                PORT_PROTOCOL_COUNT[dstport] = {}
-            if not protocol in PORT_PROTOCOL_COUNT[dstport]:
-                PORT_PROTOCOL_COUNT[dstport][protocol] = 0
-            PORT_PROTOCOL_COUNT[dstport][protocol] += 1
+    def output_port_protocol_result(self):
+        """
+        Write the port protocol count to a file port_protocol.csv
+        """
+        with open(PORT_PROTOCOL_FILENAME, 'w') as csvfile:
+            fieldnames = ['Port', 'Protocol', 'Count']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
             
-            # check if the tag exist in our mapping
-            if not dstport in LOOKUP or not protocol in LOOKUP[dstport]:
-                print(f"Not known dstport {line}")
-                UNTAGGED_FLOW_LOGS += 1
-                continue
-
-            # lookup the tag
-            tag = LOOKUP[dstport][protocol]
-
-            if not tag in FLOW_LOGS:
-                FLOW_LOGS[tag] = 0
-
-            # inrement the tag count
-            FLOW_LOGS[tag] += 1
-
-def output_flow_log_result():
-    """
-    Write the tag count output to a file tag_count.csv
-    """
-    with open(TAG_COUNT_FILENAME, 'w') as csvfile:
-        fieldnames = ['Tag', 'Count']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for tag in FLOW_LOGS:
-            writer.writerow({'Tag': tag, 'Count': FLOW_LOGS[tag]})
+            for port in PORT_PROTOCOL_COUNT:
+                for protocol in PORT_PROTOCOL_COUNT[port]:
+                    count = PORT_PROTOCOL_COUNT[port][protocol]
+                    writer.writerow({'Port': port, 'Protocol': protocol, 'Count': count})
         
-        # if untagged exist, the write the count for untagged
-        if UNTAGGED_FLOW_LOGS > 0:
-            writer.writerow({'Tag': 'Untagged', 'Count': UNTAGGED_FLOW_LOGS})
-    
-    print(f"Tag count written to {TAG_COUNT_FILENAME}")
+        print(f"Port protocol combination written to {PORT_PROTOCOL_FILENAME}")
 
-def output_port_protocol_result():
-    """
-    Write the port protocol count to a file port_protocol.csv
-    """
-    with open(PORT_PROTOCOL_FILENAME, 'w') as csvfile:
-        fieldnames = ['Port', 'Protocol', 'Count']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        
-        for port in PORT_PROTOCOL_COUNT:
-            for protocol in PORT_PROTOCOL_COUNT[port]:
-                count = PORT_PROTOCOL_COUNT[port][protocol]
-                writer.writerow({'Port': port, 'Protocol': protocol, 'Count': count})
-    
-    print(f"Port protocol combination written to {PORT_PROTOCOL_FILENAME}")
+    def run(self):
+        # prepare the protocol mapping and lookup files
+        self.parse_protocol_number()
+        self.parse_lookup()
+        self.parse_flow_log()
+        # write the results to the output files
+        self.output_flow_log_result()
+        self.output_port_protocol_result()
+
 
 def main():
     """
     Main method to trigger the parsing
     """
-    # prepare the protocol mapping and lookup files
-    parse_protocol_number()
-    parse_lookup()
-    parse_flow_log()
-    # write the results to the output files
-    output_flow_log_result()
-    output_port_protocol_result()
+    parser = FlowLogParser()
+    parser.run()
 
 if __name__ == '__main__':
     main()
